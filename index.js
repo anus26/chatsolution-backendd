a6d9354fef4082915a803ac7455dac0b1776dd15
anus26 <anusraza268gmail.com>
2025-06-20 19:02:21 -0700

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://chat-solution-isoj.vercel.app',
   credentials: true // agar cookies bhejni hain to
 }));
 app.use(express.json())
